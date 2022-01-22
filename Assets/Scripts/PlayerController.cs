72cac4dc2a8ba912ed9d75aa58a39eb688eae6e1
José Gabriel González <jgg032899@gmail.com>
2022-01-22 00:36:55 -0400

    Animating the player
    Added Idle, run and jump animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,20 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D theRB;
     public float jumpForce;
 
+    private bool isGrounded;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+
+    private bool canDoubleJump;
+
+    private Animator anim;
+    private SpriteRenderer theSR;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        anim = GetComponent<Animator>();
+        theSR = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -19,9 +29,39 @@ public class PlayerController : MonoBehaviour
     {
         theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
 
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
         if (Input.GetButtonDown("Jump"))
         {
-            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            if (isGrounded)
+            {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }
+            else
+            {
+                if (canDoubleJump)
+                {
+                    theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                    canDoubleJump = false;
+                }
+            }
         }
+
+        if (theRB.velocity.x < 0)
+        {
+            theSR.flipX = true;
+        }
+        else if (theRB.velocity.x > 0)
+        {
+            theSR.flipX = false;
+        }
+
+        anim.SetFloat("moveSpeed", Mathf.Abs(theRB.velocity.x));
+        anim.SetBool("isGrounded", isGrounded);
     }
 }
