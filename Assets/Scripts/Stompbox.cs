e1bedc72266c58629680fd3b3f5521d1ac811d02
José Gabriel González <jgg032899@gmail.com>
2022-07-02 01:54:48 -0400

    Adding stomp mechanic

---
+++ Assets/Scripts/Stompbox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stompbox : MonoBehaviour
+{
+    public GameObject deathEffect;
+    public GameObject collectible;
+
+    [Range(0, 100)]
+    public float changeToDrop;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.transform.parent.gameObject.SetActive(false);
+
+            Instantiate(deathEffect, other.transform.position, other.transform.rotation);
+
+            PlayerController.instance.Bounce();
+
+            float dropSelect = Random.Range(0, 100f);
+
+            if(dropSelect <= changeToDrop)
+            {
+                Instantiate(collectible, other.transform.position, other.transform.rotation);
+            }
+        }
+    }
+}
